08c274bc090e8a94a3ce4f04998d1dde98ff0e62
Ethan Wright <wrightet707@gmail.com>
2020-11-09 20:32:53 -0800

    add user and session routes

--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :show]
+  end
+  resource :session, only: [:create, :new, :destroy]
 end
