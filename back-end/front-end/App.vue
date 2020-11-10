13e0e229229838f21d57ab54b5b7dfbfee137598
Ethan Wright <wrightet707@gmail.com>
2020-11-10 15:11:08 -0800

    move frontend folder inside back end, begin testing front end

--- back-end/front-end/App.vue
+++ back-end/front-end/App.vue
@@ -2,9 +2,14 @@
   <div><h1>This is a Vue app.</h1></div>
 </template>
 <script>
+import helloworld from './components/helloworld';
 export default {
   name: "app",
+  components:{
+    helloworld
+  }
 };
 </script>
 <style lang="scss" scoped>
+
 </style>
---
+++ back-end/front-end/components/helloworld.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class='hello'>
+        <h1>Hello</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'helloworld',
+    props: {
+        message: String
+    }
+}
+</script>
