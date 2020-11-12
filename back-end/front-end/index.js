67b9a2d529033189e3e9e1807bd2a95632d8f0ee
Ethan Wright <wrightet707@gmail.com>
2020-11-12 15:07:33 -0800

    render index correctly

--- back-end/front-end/index.js
+++ back-end/front-end/index.js
@@ -3,13 +3,8 @@ import App from './App.vue';
 // import router from './router'
 
 Vue.config.productionTip = false;
-// new Vue({
-//   render(h) {
-//     return h(App)
-//   }
-// }).$mount('#root')
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+  render(h) {
+    return h(App)
+  }
 }).$mount('#app')
