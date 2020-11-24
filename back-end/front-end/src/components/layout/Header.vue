6c9e0753e5d0171a7ece1d93c5fb583a7927f80b
Ethan Wright <wrightet707@gmail.com>
2020-11-23 16:36:35 -0800

    header

---
+++ back-end/front-end/src/components/layout/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class='header'>
+        <h1>Welcome to this super sweet App</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+    .header{
+        display: flex;
+        align-items: center;
+    }
+</style>
--- back-end/front-end/src/views/Home.vue
+++ back-end/front-end/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
- 
+    <Header/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
+import Header from '../components/layout/Header.vue';
 export default {
   name: 'Home',
   components: {
-  
+    Header
   }
 }
 </script>
